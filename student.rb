81ca93af109100122763b1177a8a6e8fa43af685
Ashnaali3255 <ashnaalipk@gmail.com>
2023-07-04 15:13:09 +0500

    Update student.rb file

--- student.rb
+++ student.rb
@@ -3,6 +3,7 @@ require_relative 'person'
 class Student < Person
   def initialize(age, _classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
+    @classroom = classroom
   end
 
   def play_hooky
