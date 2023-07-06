f5bf297be8863c58e98d3ee748dfe94a0deace01
Ashnaali3255 <ashnaalipk@gmail.com>
2023-07-06 18:23:38 +0500

    Update this file

--- app.rb
+++ app.rb
@@ -114,5 +114,53 @@ class App
     puts ''
   end
 
-  def quit_app; end
+  def run
+    puts 'Welcome to the Console App!'
+    puts ''
+
+    loop do
+      show_menu
+      choice = read_choice
+
+      case choice
+      when 1
+        list_books
+      when 2
+        list_people
+      when 3
+        create_person
+      when 4
+        create_book
+      when 5
+        create_rental
+      when 6
+        list_rentals_for_person
+      when 7
+        quit_app
+      else
+        puts 'Invalid choice. Please try again.'
+      end
+    end
+  end
+
+  private
+
+  def show_menu
+    puts 'Please select an option:'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List rentals for a person'
+    puts '7. Quit'
+  end
+
+  def read_choice
+    gets.chomp.to_i
+  end
 end
+
+app = App.new
+app.run
+
--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
-require './app'
+require_relative 'app'
+
 def main
   app = App.new
   app.run
 end
+
 main
